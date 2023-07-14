9e9bf5eb1808a2fa5270ad831ff91ca1c500c7c3
aziz <mraziz.aden@gmail.com>
2023-07-13 21:01:52 -0400

    models

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag.js');
+const ProductTag = require('./ProductTag');
+
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id',
+});
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
